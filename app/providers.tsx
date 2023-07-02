a6a9ab9356032b7a1b4f87bc26afe82ee51b38a1
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-02 00:30:52 -0400

    Use context theme and component header, footer

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+"use client"
+import Footer from "@/components/Footer"
+import Header from "@/components/Header"
+import { ThemeProvider } from "next-themes"
+import React from "react"
+
+const Providers = ({children}: {children: React.ReactNode}) => {
+    return (
+        <ThemeProvider attribute="class" enableSystem={true} enableColorScheme={false}>
+            <Header/>
+            <main className="dark:bg-gray-900">{children}</main>
+            <Footer/>
+        </ThemeProvider>
+    )
+}
+
+export default Providers
