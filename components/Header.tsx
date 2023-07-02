ca4446450d4b8568a8fc48306c9587094a846f75
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-02 18:44:09 -0400

    Use component AuthProvider

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,11 @@ import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import ThemeSwitcher from "./ThemeSwitcher";
+import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Header = () => {
-    const session = {};
+const Header = async () => {
+    const session = null;
 
     return (
         <nav className="flexBetween navbar dark:bg-gray-900">
@@ -34,7 +36,7 @@ const Header = () => {
                         <Link href="/create-project">Compartir proyecto</Link>
                     </>
                 ) : (
-                    <div>xd</div>
+                    <AuthProviders />
                 )}
             </div>
         </nav>
