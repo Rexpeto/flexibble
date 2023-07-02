b68043c3f0aa707d0f97c58fd1bc604628e52837
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-01 22:28:30 -0400

    Constast text

---
+++ constants/index.ts
@@ -0,0 +1,95 @@
+export const NavLinks = [
+  { href: '/', key: 'Inspiration', text: 'Inspiración' },
+  { href: '/', key: 'Find Projects', text: 'Buscar Proyectos' },
+  { href: '/', key: 'Learn Development', text: 'Aprender Desarrollo' },
+  { href: '/', key: 'Career Advancement', text: 'Adelanto de la carrera' },
+  { href: '/', key: 'Hire Developers', text: 'Contratar Desarrolladores' }
+];
+
+export const categoryFilters = [
+  "Frontend",
+  "Backend",
+  "Full-Stack",
+  "Mobile",
+  "UI/UX",
+  "Game Dev",
+  "DevOps",
+  "Data Science",
+  "Machine Learning",
+  "Cybersecurity",
+  "Blockchain",
+  "E-commerce",
+  "Chatbots"
+]
+
+export const footerLinks = [
+  {
+    title: 'Para Programadores',
+    links: [
+      'Hazte pro!',
+      'Explora Trabajos',
+      'Blogs de programación',
+      'Code podcast',
+      'Proyectos Open-source',
+      'Recomendar amigo',
+      'Código de conducta',
+    ],
+  },
+  {
+    title: 'Hazte Programador',
+    links: [
+      'Pública oferta',
+      'Pública oferta Freelance',
+      'Buscar un Desarrollador',
+    ],
+  },
+  {
+    title: 'Marcas',
+    links: [
+      'Publicidad con nosotros',
+    ],
+  },
+  {
+    title: 'Compañia',
+    links: [
+      'Acerca',
+      'Carreras',
+      'Soporte',
+      'Testimoniales',
+      'API',
+      'Términos de Servicio',
+      'Política de Privacidad',
+      'Política de Cookies',
+    ],
+  },
+  {
+    title: 'Directorios',
+    links: [
+      'Trabajos de Desarrollador',
+      'Desarrolladores de Alquiler',
+      'Desarrolladores Freelance',
+      'Tags',
+      'Lugares',
+    ],
+  },
+  {
+    title: 'Desarrolladores Activos',
+    links: [
+      'Code Marketplace',
+      'GitHub Marketplace',
+      'NPM Registry',
+      'Packagephobia',
+    ],
+  },
+  {
+    title: 'Recursos de Desarrolladores',
+    links: [
+      'Freelancing',
+      'Desarrolladores Contratados',
+      'Portafolio de Desarrolladores',
+      'Educación de Desarrolladores',
+      'Creatividad progresiva',
+      'Treding de la industria',
+    ],
+  },
+];
