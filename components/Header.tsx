80bd770ece58c83d4b7f61ab2189587f5ce3bb42
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-07-02 02:16:10 -0400

    Use nav-link

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,22 +4,30 @@ import Link from "next/link";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 const Header = () => {
-    const session = {}
+    const session = {};
 
     return (
         <nav className="flexBetween navbar dark:bg-gray-900">
             <div className="flex-1 flexStart gap-10">
                 <Link href="/">
-                    <Image className="dark:invert" src="/logo.svg" width={115} height={43} alt="Flexibble" />
+                    <Image
+                        className="dark:invert"
+                        src="/logo.svg"
+                        width={115}
+                        height={43}
+                        alt="Flexibble"
+                    />
                 </Link>
                 <ul className="xl:flex hidden text-small gap-7">
                     {NavLinks.map((link, index) => (
-                        <Link key={link.key} href={link.href}>{link.text}</Link>
+                        <Link className="nav-link" key={link.key} href={link.href}>
+                            {link.text}
+                        </Link>
                     ))}
                 </ul>
             </div>
             <div className="flexCenter gap-4">
-                <ThemeSwitcher/>
+                <ThemeSwitcher />
                 {session ? (
                     <>
                         PhotoUser
